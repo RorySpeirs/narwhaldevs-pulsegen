dfa6d5a3ef8d73e37d9feecf12bd6d47279a1414
Rory Speirs <roryspeirs@gmail.com>
2021-06-01 22:36:16 +1000

    Initial validation complete
    The validation seems to work pretty well. I should include the effect of goto's but this will require a fair bit of work.

--- tests/hardware_validation.py
+++ tests/hardware_validation.py
@@ -85,8 +85,8 @@ def setup_scope(scope, Ch1=True, Ch2=False, pre_trig_record=0.5E-3):
     # scope.write(':TFORce')
     time.sleep(0.5)
 
-def random_sequence(pg):
-    np.random.seed(seed=19870909)   #Seed it so the sequence is the same every time i run it.
+def random_sequence(pg, seed=19870909):
+    np.random.seed(seed=seed)   #Seed it so the sequence is the same every time i run it.
 
     instruction_num = 8192
     states = np.empty((instruction_num, 24), dtype=np.int)
@@ -113,49 +113,115 @@ if __name__ == "__main__":
     scope = rigol_ds1202z_e.RigolScope()
     # scope.default_setup(Ch1=True, Ch2=False, pre_trig_record=0.5E-6)
     # setup_scope(scope, Ch1=True, Ch2=False, pre_trig_record=0.5E-6)
-    scope.write(':SINGLE')  #Once setup has been done once, you can just re-aquire with same settings. It is faster.
 
     pg = ndpulsegen.PulseGenerator()
     assert pg.connect_serial()
-    # pg.write_action(reset_output_coordinator=True)
+    # pg.write_action(reset_output_coordinator=True) 
 
-    # simple_sequence(pg)
-    # # software_trig(pg)
-    durations, states = random_sequence(pg)
-    pg.write_action(trigger_now=True)
-    pg.read_all_messages(timeout=0.1)
-    # print(pg.get_state())
+    for trial in range(50):
+        print(f'Trial {trial}')
+        scope.write(':SINGLE')  #Once setup has been done once, you can just re-aquire with same settings. It is faster.
 
-    # t_programmed = np.cumsum(durations)*10E-9 + 0.45E-6 #This isnt right. Think harder about it.
-    # V_programmed = states[:,0]
-    # plot(t_programmed, V_programmed)
-    # show()
-    # print(durations[:17])
-    # print(np.cumsum(durations)[:17])
-    # print(V_programmed[:17])
-    dt = 1E-10
-    tsim = [-dt]
-    Vsim = [0]
-    tcum = 0
-    for duration, state in zip(durations, states[:, 0]):
-        tsim.append(tcum)
-        Vsim.append(state)
-        tcum += duration*10E-9
-        tsim.append(tcum-dt)
-        Vsim.append(state)
-    tsim = np.array(tsim)
-    Vsim = np.array(Vsim)*2+0.1
+        # simple_sequence(pg)
+        # # software_trig(pg)
+        durations, states = random_sequence(pg, seed=trial)
+        pg.write_action(trigger_now=True)
+        pg.read_all_messages(timeout=0.1)
+        # print(pg.get_state())
 
-    t, V = scope.read_data(channel=1, duration=210E-6)
-    
-    zero_idx = np.argmax(V > 0.6)
-    t = t - t[zero_idx]
+        # Obtain the data from the scope
+        time.sleep(0.1)
+        t, V = scope.read_data(channel=1, duration=210E-6)
+        t = t - t[np.argmax(V > 0.6)]   # Zeros the time on the first transition. Be careful, this may have some thresholding impact?
+
+        # Simulate what the pulse sequence should be
+        chan = 0    #Select the Channel of the Pulse generator you are looking at
+        dt = 1E-10
+        tsim = [-dt]
+        Vsim = [0]
+        tcum = 0
+        for duration, state in zip(durations, states[:, chan]):
+            tsim.append(tcum + dt)
+            Vsim.append(state)
+            tcum += duration*10E-9
+            tsim.append(tcum - dt)
+            Vsim.append(state)
+        tsim = np.array(tsim)
+        Vsim = np.array(Vsim)*2+0.1
+        
+        # Extract the rise and fall times of the simulated structure. Note, this is different to the duratiosn, because the state doesnt change at each instruction
+        rise_tsim = []
+        fall_tsim = []
+        for Va, Vb, ta, tb in zip(Vsim[:-1], Vsim[1:], tsim[:-1], tsim[1:]):
+            if Vb > Va:
+                rise_tsim.append((ta+tb)/2)
+            if Vb < Va:
+                fall_tsim.append((ta+tb)/2)
+
+        rise_tsim = np.array(rise_tsim)
+        fall_tsim = np.array(fall_tsim)
+
+        # Extract the rise and fall times of the scope data. I do a linear interpolation to find the crossing time.
+        threshold = 0.3 # Good to set a low threshold because I am looking for those runty skinny spikes that might possibly appear. But it does make the jitter appear bigger than the actual value.
+        # threshold = 1.1
+        dt = 1E-9
+        thresholded = np.zeros(V.size)
+        thresholded[V < threshold] = 0
+        thresholded[V >= threshold] = 1
+        rise_tdata = []
+        fall_tdata = []
+        for Ta, Tb, Va, Vb, ta, tb in zip(thresholded[:-1], thresholded[1:], V[:-1], V[1:], t[:-1], t[1:]):
+            if Tb > Ta:
+                m = (Vb-Va)/dt
+                t_cross = (threshold - Va + m*ta)/m
+                rise_tdata.append(t_cross)
+            if Tb < Ta:
+                m = (Vb-Va)/dt
+                t_cross = (threshold - Va + m*ta)/m
+                fall_tdata.append(t_cross)
+
+        rise_tdata = np.array(rise_tdata)
+        fall_tdata = np.array(fall_tdata)
+
+        # # Plot the time domain signal of the simulated and obtained signals
+        # plot(t*1E6, V, label='ch0')
+        # plot(tsim*1E6, Vsim, label='ch0 sim')
+        # xlabel('time (μs)')
+        # ylabel('output (V)')
+        # legend()
+        # show()
+
+        # is there the same number of rise and fall times in the data as in the sim? If not, there are big problems, and the remaining analysis wont work
+        print(f'No. rising transitions (sim/actual): {rise_tsim.size}/{rise_tdata.size}')
+        print(f'No. falling transitions (sim/actual): {fall_tsim.size}/{fall_tdata.size}')
+
+        rise_error = rise_tsim - rise_tdata
+        fall_error = fall_tsim - fall_tdata
+        # Now remove the linear trend, since that just indicates an offset in the clocks of the Pulse generator and scope, which would be fixed if the scope had a sync in/out
+        m_rise, c_rise = np.polyfit(rise_tsim, rise_error, 1)
+        rise_error_residuals = rise_error - (m_rise*rise_tsim + c_rise)
+        m_fall, c_fall = np.polyfit(fall_tsim, fall_error, 1)
+        fall_error_residuals = fall_error - (m_fall*fall_tsim + c_fall)
+
+        # print('Rise time jitter (std)= {}ps'.format(round(np.std(rise_error_residuals)*1E12)))
+        # print('Fall time jitter (std)= {}ps'.format(round(np.std(fall_error_residuals)*1E12)))
+        # Standard deviation and RMS are identical under these conditions. Trust me, I checked.
+        print('Rise time jitter (RMS)={}ps. (Max)={}ps'.format(round(np.sqrt(np.mean(rise_error_residuals**2))*1E12), round(np.max(np.abs(rise_error_residuals))*1E12)))
+        print('Fall time jitter (RMS)={}ps. (Max)={}ps'.format(round(np.sqrt(np.mean(fall_error_residuals**2))*1E12), round(np.max(np.abs(fall_error_residuals))*1E12)))
+        if np.any(np.abs(rise_error_residuals) > 0.7E-9):
+            print(f'Problem: a rise error is too big')
+            break
+        if np.any(np.abs(fall_error_residuals) > 0.8E-9):
+            print(f'Problem: a fall error is too big')
+            break
+
+        # plot(rise_tsim*1E6, rise_error*1E9, 'C0', label='rising edge')
+        # plot(fall_tsim*1E6, fall_error*1E9, 'C1', label='falling edge')
+        # plot(rise_tsim*1E6, rise_error_residuals*1E9, 'C0', alpha=0.5, label='rising edge residuals')
+        # plot(fall_tsim*1E6, fall_error_residuals*1E9, 'C1', alpha=0.5, label='falling edge residuals')
+        # xlabel('time (μs)')
+        # ylabel('Error (ns)')
+        # legend()
+        # show()
 
 
-    plot(t*1E6, V, label='ch0')
-    plot(tsim*1E6, Vsim, label='ch0 sim')
-    # xlabel('time (ns)')
-    xlabel('time (μs)')
-    ylabel('output (V)')
-    legend()
-    show()
