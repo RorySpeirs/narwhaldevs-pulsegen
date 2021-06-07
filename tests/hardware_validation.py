9bc356a7c8ef02972ca9f6f4e078eba9b7f5f7e6
Rory Speirs <roryspeirs@gmail.com>
2021-06-07 10:46:40 +1000

    more tweaks on hardware validation

--- tests/hardware_validation.py
+++ tests/hardware_validation.py
@@ -1,6 +1,7 @@
 import numpy as np
 import time
 import random
+from numba import jit
 import struct
 import sys
 import os
@@ -38,6 +39,10 @@ def setup_scope(scope, Ch1=True, Ch2=False, pre_trig_record=0.5E-3):
     on_off = {True:'ON', False:'OFF'}
     scope.write(f':CHANnel1:DISPlay {on_off[Ch1]}')
     scope.write(f':CHANnel2:DISPlay {on_off[Ch2]}')
+    if Ch1:
+        trigchan='CHANnel1'
+    else:
+        trigchan='CHANnel2'
 
     scope.write(':RUN')
     scope.write(':CHANnel1:BWLimit OFF')    
@@ -58,6 +63,14 @@ def setup_scope(scope, Ch1=True, Ch2=False, pre_trig_record=0.5E-3):
     scope.write(':CHANnel2:SCALe 0.5')
     scope.write(':CHANnel2:VERNier OFF')
 
+    scope.write(':TRIGger:MODE EDGE')
+    scope.write(':TRIGger:COUPling DC')
+    scope.write(':TRIGger:HOLDoff 16E-9')
+    scope.write(':TRIGger:NREJect OFF')
+    scope.write(f':TRIGger:EDGe:SOURce {trigchan}')  #EXT, CHANnel1, CHANnel2, AC
+    scope.write(':TRIGger:EDGE:SLOPe POSitive')
+    scope.write(':TRIGger:EDGE:LEVel 1.0')
+
     scope.write(':CURSor:MODE OFF')
     scope.write(':MATH:DISPlay OFF')
     scope.write(':REFerence:DISPlay OFF')
@@ -71,14 +84,6 @@ def setup_scope(scope, Ch1=True, Ch2=False, pre_trig_record=0.5E-3):
     sample_rate = float(scope.query(':ACQuire:SRATe?'))
     scope.write(f':TIMebase:MAIN:OFFSet {0.5*memory_depth/sample_rate - pre_trig_record}')     # Determines where to start recording points. The default is to record equally either side of the triggger. The last nummer added on here is how long to record before the trigger. P1-123.
     scope.write(':TIMebase:DELay:ENABle OFF')
-
-    scope.write(':TRIGger:MODE EDGE')
-    scope.write(':TRIGger:COUPling DC')
-    scope.write(':TRIGger:HOLDoff 16E-9')
-    scope.write(':TRIGger:NREJect OFF')
-    scope.write(':TRIGger:EDGe:SOURce CHANnel1')  #EXT, CHANnel1, CHANnel2, AC
-    scope.write(':TRIGger:EDGE:SLOPe POSitive')
-    scope.write(':TRIGger:EDGE:LEVel 1.0')
 
     # scope.write(':RUN')
     # scope.write(':STOP')
@@ -173,47 +178,116 @@ def simulate_output_coordinator(instructions):
     return np.array(durations, dtype=np.int), np.array(states, dtype=np.int)
 #####################################################################################################################
 
+@jit(nopython=True, cache=True)
+def find_transition_times_data(V, t, threshold = 0.3):
+    dt = t[1]-t[0]
+    N_rise = 0
+    N_fall = 0
+    rise_tdata = np.zeros(int(V.size/5))
+    fall_tdata = np.zeros(int(V.size/5))
+    for idx in range(V.size - 1):
+        ta = t[idx]
+        Va = V[idx]
+        Vb = V[idx+1]
+        if Va <= threshold and Vb > threshold:
+            #rising edge
+            m = (Vb-Va)/dt
+            t_cross = (threshold - Va + m*ta)/m
+            rise_tdata[N_rise] = t_cross
+            N_rise += 1
+        if Vb <= threshold and Va > threshold:
+            #falling edge
+            m = (Vb-Va)/dt
+            t_cross = (threshold - Va + m*ta)/m
+            fall_tdata[N_fall] = t_cross
+            N_fall += 1
+    return rise_tdata[:N_rise], fall_tdata[:N_fall]
+
+# def find_transition_times_sim(durations, states, pulsegen_chan):
+#     print('Finding simulated transition times...')
+#     # Simulate what the pulse sequence should be
+#     dt = 1E-10
+#     tsim = np.zeros(durations.size*2+1)
+#     Vsim = np.zeros(durations.size*2+1)
+#     tsim[0] = -dt
+#     Vsim[0] = 0
+#     # Up to here
+#     tcum = 0
+#     for duration, state in zip(durations, states[:, pulsegen_chan]):
+#         tsim.append(tcum + dt)
+#         Vsim.append(state)
+#         tcum += duration*10E-9
+#         tsim.append(tcum - dt)
+#         Vsim.append(state)
+#     tsim = np.array(tsim)
+#     Vsim = np.array(Vsim)*2+0.1
+#     tsim = tsim - tsim[np.argmax(Vsim > 0.5)]   # Zeros the time on the first transition. 
+    
+#     # Extract the rise and fall times of the simulated structure. Note, this is different to the duratiosn, because the state doesnt change at each instruction
+#     rise_tsim = []
+#     fall_tsim = []
+#     for Va, Vb, ta, tb in zip(Vsim[:-1], Vsim[1:], tsim[:-1], tsim[1:]):
+#         if Vb > Va:
+#             rise_tsim.append((ta+tb)/2)
+#         if Vb < Va:
+#             fall_tsim.append((ta+tb)/2)
+
+#     rise_tsim = np.array(rise_tsim)
+#     fall_tsim = np.array(fall_tsim)
+
 
 
 if __name__ == "__main__":
     scope = rigol_ds1202z_e.RigolScope()
     # scope.default_setup(Ch1=True, Ch2=False, pre_trig_record=0.5E-6)
 
-    setup_scope(scope, Ch1=True, Ch2=False, pre_trig_record=250E-9)
-    # setup_scope(scope, Ch1=True, Ch2=True, pre_trig_record=250E-9)
+    # setup_scope(scope, Ch1=False, Ch2=True, pre_trig_record=10E-6)
+    # setup_scope(scope, Ch1=True, Ch2=False, pre_trig_record=10E-6)
+    setup_scope(scope, Ch1=True, Ch2=True, pre_trig_record=10E-6)
 
     pg = ndpulsegen.PulseGenerator()
     assert pg.connect_serial()
     # pg.write_action(reset_output_coordinator=True) 
 
-    pulse_number_errors = 0
-    pulse_timing_errors = 0
 
-    seed_salt = 5481
-    for trial in range(5, 6):
+
+    '''Transition number errors
+    Pulse_gen_chan  random_seed
+    11              5486
+    3               871
+
+    '''
+    scope_channels = [1, 2]
+    pulsegen_channels = [2, 3]
+    # scope_channels = [2]
+    # pulsegen_channels = [3]
+
+
+    transition_number_errors = 0
+    pulse_timing_errors = 0
+    # for trial, rand_seed in enumerate([871]):
+    seed_salt = 865
+    for trial in range(10):
+        rand_seed = trial+seed_salt
         print(f'Trial {trial}')
         scope.write(':SINGLE')  #Once setup has been done once, you can just re-aquire with same settings. It is faster.
 
         print('Generating sequence...')
-        durations, states = random_sequence(pg, seed=trial+seed_salt)
+        durations, states = random_sequence(pg, seed=rand_seed)
         sequence_duration = durations.sum()*10E-9
         print('Sequence duration = {}ms'.format(sequence_duration*1E3))
-        if sequence_duration >= 24E-3 - 500E-9: #I want 250ns at the start and end
+        if sequence_duration >= 24E-3 - 10.250E-6: #I want 1us at the start at the start and 250ns end
             print(f'Sequence too long. Skipping trial {trial}')
             continue
 
         pg.write_action(trigger_now=True)
         pg.read_all_messages(timeout=0.1)
 
-        # scope_channels = [1, 2]
-        # pulsegen_channels = [11, 22]
-        scope_channels = [1]
-        pulsegen_channels = [11]
         for scope_chan, pulsegen_chan in zip(scope_channels, pulsegen_channels):
             # Obtain the data from the scope
             time.sleep(0.1)
             print(f'Reading data from scope channel {scope_chan}...')
-            t, V = scope.read_data(channel=scope_chan, duration=sequence_duration+500E-9)
+            t, V = scope.read_data(channel=scope_chan, duration=sequence_duration+10.25E-6)
             t = t - t[np.argmax(V > 0.6)]   # Zeros the time on the first transition. Be careful, this may have some thresholding impact?
 
             print('Finding simulated transition times...')
@@ -246,26 +320,8 @@ if __name__ == "__main__":
 
             print('Finding scope data transition times...')
             # Extract the rise and fall times of the scope data. I do a linear interpolation to find the crossing time.
-            threshold = 0.3 # Good to set a low threshold because I am looking for those runty skinny spikes that might possibly appear. But it does make the jitter appear bigger than the actual value.
-            # threshold = 1.1
-            dt = t[1] - t[0]
-            thresholded = np.zeros(V.size)
-            thresholded[V < threshold] = 0
-            thresholded[V >= threshold] = 1
-            rise_tdata = []
-            fall_tdata = []
-            for Ta, Tb, Va, Vb, ta, tb in zip(thresholded[:-1], thresholded[1:], V[:-1], V[1:], t[:-1], t[1:]):
-                if Tb > Ta:
-                    m = (Vb-Va)/dt
-                    t_cross = (threshold - Va + m*ta)/m
-                    rise_tdata.append(t_cross)
-                if Tb < Ta:
-                    m = (Vb-Va)/dt
-                    t_cross = (threshold - Va + m*ta)/m
-                    fall_tdata.append(t_cross)
-
-            rise_tdata = np.array(rise_tdata)
-            fall_tdata = np.array(fall_tdata)
+            # Good to set a low threshold because I am looking for those runty skinny spikes that might possibly appear. But it does make the jitter appear bigger than the actual value.
+            rise_tdata, fall_tdata = find_transition_times_data(V, t, threshold = 0.3)
 
             # # Plot the time domain signal of the simulated and obtained signals
             # min_t = 400E-6
@@ -285,11 +341,14 @@ if __name__ == "__main__":
             print(f'Pulsegen channel {pulsegen_chan} no. rising transitions (sim/actual): {rise_tsim.size}/{rise_tdata.size}')
             print(f'Pulsegen channel {pulsegen_chan} no. falling transitions (sim/actual): {fall_tsim.size}/{fall_tdata.size}')
 
+            transition_number_error = False
             if rise_tsim.size != rise_tdata.size or fall_tsim.size != fall_tdata.size:
-                pulse_number_errors += 1
+                transition_number_error = True
+                transition_number_errors += 1
                 last_err = 0
                 list_size = min((rise_tsim.size, rise_tdata.size))
                 sim_idx, data_idx = 0, 0
+                print('Transition errors at following times:')
                 while sim_idx < list_size:
                     t_sim = rise_tsim[sim_idx]
                     t_data = rise_tdata[data_idx]
@@ -299,64 +358,67 @@ if __name__ == "__main__":
                     if abs(err - last_err) > 3E-9:
                         print(t_data)
                         data_idx += 1
-                        # break
+                        if data_idx > 50:
+                            print('too many errors, ending program')
+                            break
                     else:
                         last_err = err
                     data_idx += 1
                     sim_idx += 1
 
                 ''' Remember, even if this really is some stability problem in the FPGA, maybe you can fix it. For example, pipline all the outputs.'''
-                print('Pulse number error!')
-            t_data = 0.001752345439938187
-            min_t = t_data - 50E-6
-            max_t = t_data + 50E-6
-            min_idx = np.argmax(t > min_t)
-            min_idx_sim = np.argmax(tsim > min_t)
-            max_idx = np.argmax(t > max_t)
-            max_idx_sim = np.argmax(tsim > max_t)
-            plot(t[min_idx:max_idx]*1E6, V[min_idx:max_idx], label='ch0')
-            plot(tsim[min_idx_sim:max_idx_sim]*1E6, Vsim[min_idx_sim:max_idx_sim], label='ch0 sim')
-            xlabel('time (μs)')
-            ylabel('output (V)')
-            legend()
-            show()
-            break
+   
+                t_plot_centre = 0.001752345439938187
+                t_plot_range = 100E-6
+                min_t = t_plot_centre - t_plot_range/2
+                max_t = t_plot_centre + t_plot_range/2
+                min_idx = np.argmax(t > min_t)
+                min_idx_sim = np.argmax(tsim > min_t)
+                max_idx = np.argmax(t > max_t)
+                max_idx_sim = np.argmax(tsim > max_t)
+                plot(t[min_idx:max_idx]*1E6, V[min_idx:max_idx], label='scope data')
+                plot(tsim[min_idx_sim:max_idx_sim]*1E6, Vsim[min_idx_sim:max_idx_sim], label='sim')
+                xlabel('time (μs)')
+                ylabel('output (V)')
+                legend()
+                show()
+            
+
+            if not transition_number_error:
+                rise_error = rise_tsim - rise_tdata
+                fall_error = fall_tsim - fall_tdata
+                # Now remove the overall trend, since that just indicates an offset in the clocks of the Pulse generator and scope, which would be fixed if the scope had a sync in/out
+                polynomial_order = 1
+                p_rise = np.polyfit(rise_tsim, rise_error, polynomial_order)
+                rise_error_residuals = rise_error - np.polyval(p_rise, rise_tsim)
+                p_fall = np.polyfit(fall_tsim, fall_error, polynomial_order)
+                fall_error_residuals = fall_error - np.polyval(p_fall, fall_tsim)
+
+                # print('Rise time jitter (std)= {}ps'.format(round(np.std(rise_error_residuals)*1E12)))
+                # print('Fall time jitter (std)= {}ps'.format(round(np.std(fall_error_residuals)*1E12)))
+                # Standard deviation and RMS are identical under these conditions. Trust me, I checked.
+                print('Rise time jitter (RMS)={}ps. (Max)={}ps'.format(round(np.sqrt(np.mean(rise_error_residuals**2))*1E12), round(np.max(np.abs(rise_error_residuals))*1E12)))
+                print('Fall time jitter (RMS)={}ps. (Max)={}ps'.format(round(np.sqrt(np.mean(fall_error_residuals**2))*1E12), round(np.max(np.abs(fall_error_residuals))*1E12)))
+                # if np.any(np.abs(rise_error_residuals) > 0.7E-9):
+                #     print(f'Problem: a rise error is too big')
+                #     break
+                # if np.any(np.abs(fall_error_residuals) > 0.8E-9):
+                #     print(f'Problem: a fall error is too big')
+                #     break
+                if np.any(np.abs(rise_error_residuals) > 2E-9):
+                    pulse_timing_errors += 1
+                if np.any(np.abs(fall_error_residuals) > 2E-9):
+                    pulse_timing_errors += 1
 
 
-            rise_error = rise_tsim - rise_tdata
-            fall_error = fall_tsim - fall_tdata
-            # Now remove the overall trend, since that just indicates an offset in the clocks of the Pulse generator and scope, which would be fixed if the scope had a sync in/out
-            polynomial_order = 1
-            p_rise = np.polyfit(rise_tsim, rise_error, polynomial_order)
-            rise_error_residuals = rise_error - np.polyval(p_rise, rise_tsim)
-            p_fall = np.polyfit(fall_tsim, fall_error, polynomial_order)
-            fall_error_residuals = fall_error - np.polyval(p_fall, fall_tsim)
-
-            # print('Rise time jitter (std)= {}ps'.format(round(np.std(rise_error_residuals)*1E12)))
-            # print('Fall time jitter (std)= {}ps'.format(round(np.std(fall_error_residuals)*1E12)))
-            # Standard deviation and RMS are identical under these conditions. Trust me, I checked.
-            print('Rise time jitter (RMS)={}ps. (Max)={}ps'.format(round(np.sqrt(np.mean(rise_error_residuals**2))*1E12), round(np.max(np.abs(rise_error_residuals))*1E12)))
-            print('Fall time jitter (RMS)={}ps. (Max)={}ps'.format(round(np.sqrt(np.mean(fall_error_residuals**2))*1E12), round(np.max(np.abs(fall_error_residuals))*1E12)))
-            # if np.any(np.abs(rise_error_residuals) > 0.7E-9):
-            #     print(f'Problem: a rise error is too big')
-            #     break
-            # if np.any(np.abs(fall_error_residuals) > 0.8E-9):
-            #     print(f'Problem: a fall error is too big')
-            #     break
-            if np.any(np.abs(rise_error_residuals) > 2E-9):
-                pulse_timing_errors += 1
-            if np.any(np.abs(fall_error_residuals) > 2E-9):
-                pulse_timing_errors += 1
-
-
-            plot(rise_tsim*1E6, rise_error*1E9, 'C0', label='rising edge')
-            plot(fall_tsim*1E6, fall_error*1E9, 'C1', label='falling edge')
-            plot(rise_tsim*1E6, rise_error_residuals*1E9, 'C0', alpha=0.5, label='rising edge residuals')
-            plot(fall_tsim*1E6, fall_error_residuals*1E9, 'C1', alpha=0.5, label='falling edge residuals')
-            xlabel('time (μs)')
-            ylabel('Error (ns)')
-            legend()
-            show()
-    print(f'Pilse number errors = {pulse_number_errors}. Pulse timing errors = {pulse_timing_errors}')
+                # plot(rise_tsim*1E6, rise_error*1E9, 'C0', label='rising edge')
+                # plot(fall_tsim*1E6, fall_error*1E9, 'C1', label='falling edge')
+                # plot(rise_tsim*1E6, rise_error_residuals*1E9, 'C0', alpha=0.5, label='rising edge residuals')
+                # plot(fall_tsim*1E6, fall_error_residuals*1E9, 'C1', alpha=0.5, label='falling edge residuals')
+                # xlabel('time (μs)')
+                # ylabel('Error (ns)')
+                # legend()
+                # show()
+    print(f'Transition number errors = {transition_number_errors}. Pulse timing errors = {pulse_timing_errors}')
 
 
